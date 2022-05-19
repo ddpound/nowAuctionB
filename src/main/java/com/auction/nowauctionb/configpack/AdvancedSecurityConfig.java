fddf96acfad1664b20a447e18b52ba5100404cdc
dpound <dpound@naver.com>
2022-05-19 21:20:38 +0900

    백엔드 쪽 토큰값 받아와서 해독 할수 있도록 코드 짜는중

--- src/main/java/com/auction/nowauctionb/configpack/AdvancedSecurityConfig.java
+++ src/main/java/com/auction/nowauctionb/configpack/AdvancedSecurityConfig.java
@@ -3,12 +3,14 @@ package com.auction.nowauctionb.configpack;
 
 // 보안에 관한 설정파일, 로그인시 필터부분을 전부 관할
 
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 
+@Order(1)
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class AdvancedSecurityConfig extends WebSecurityConfigurerAdapter {
@@ -21,9 +23,15 @@ public class AdvancedSecurityConfig extends WebSecurityConfigurerAdapter {
 
         // session은 안하는걸로 , csrf 끄기
         http
+                .cors().disable()
                 .csrf().disable()
                 .sessionManagement(session ->
-                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeRequests()
+                // 나중에 아래 코드는 꼭 체인 삭제 해줘야함 테스트용도임
+                .antMatchers("/test/**")
+                .permitAll();
+
     }
 
 }
--- src/main/java/com/auction/nowauctionb/login/controller/LoginCotroller1.java
+++ src/main/java/com/auction/nowauctionb/login/controller/LoginCotroller1.java
@@ -1,8 +1,9 @@
 package com.auction.nowauctionb.login.controller;
 
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
 
 
 // 로그인 시 토큰 인증과 권한을 받아야함
@@ -11,8 +12,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class LoginCotroller1 {
 
 
-    
 
+    // 기본 로그인도 사용할수 있으니 일단 기본로그인은 남겨두기
     @GetMapping(value = "try-login")
     public String loginTry(){
 
@@ -20,6 +21,27 @@ public class LoginCotroller1 {
         return "tryLoginSuccess";
     }
 
+
+    // get방식으로 토큰값과 유저 이메일을 가져와야함
+    // 헤더에 Authentication header이여야함 bearer 넣기
+    @GetMapping(value = "try-login-google")
+    public String loginTryGoogle(@RequestBody String credentialResponse){
+
+        System.out.println(credentialResponse);
+
+
+
+        return credentialResponse;
+    }
+
+    // 테스트코드 반드시 삭제 요망
+    @GetMapping(value = "test/try-login-google")
+    public String testLoginTryGoogle(@RequestParam int i){
+
+        System.out.println(i);
+
+        return "{success : true}";
+    }
 
 
 
